085a9da16d7dee5983f050d78a18b8ef5e6dbf10
Govind  Ghogare <131388251+GovindGhogare@users.noreply.github.com>
2023-09-04 09:34:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".multiple-text",{
+    strings: [
+        "Frontend Developer","Blogger","Youtuber","Photographer","Hacker"],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true
+
+
+})
